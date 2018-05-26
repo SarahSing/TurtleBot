9d63da73333ea880ff9eee34b52299aee891e024
SarahSing <36783545+SarahSing@users.noreply.github.com>
2018-05-26 13:57:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const bot = new Client();
 
-bot.login('NDUwMDI2MDU0MjM3NjgzNzI0.DetWrg.JaPKLt1TgjuVpEaIrLO3fMzJMnk');
+bot.login('NDE2ODMxODk2MjE2MTQxODM0.Detbnw.ldMVsCva_yQMcHPrAsnFKjt3378');
